4cb81a96db9cb777fb7a9e8aaa4fc81da0861c60
ChuyenLC <101199742+taptanhcoding@users.noreply.github.com>
2023-03-10 09:17:56 +0700

    kết nối redis cloud

--- src/configs/redis.configs.js
+++ src/configs/redis.configs.js
@@ -1,5 +1,11 @@
 const { createClient } = require("redis");
-const client = createClient();
+const client = createClient({
+  socket:{
+    host:"redis-11367.c292.ap-southeast-1-1.ec2.cloud.redislabs.com",
+    port: "11367"
+  },
+  password: "ReWGYAjycA4USGVCq0Cem5R34M8Mkz0g"
+});
 
 async function connectRedis() {
   try {
